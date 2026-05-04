cca1c3bc8caf52242db22b7bf11e3b5d8f09acf1
wwatso11 <wwatso11@wgu.edu.com>
2026-05-04 22:34:15 +0000

    Unit test 1

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,33 @@
 import pytest
+import numpy as np
+import os
+import pandas as pd
+
+from ml.model import (
+    compute_model_metrics,
+    inference,
+    load_model,
+    performance_on_categorical_slice,
+    save_model,
+    train_model,
+)
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_inference():
     """
     # add description for the first test
     """
-    # Your code here
-    pass
+    project_path = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(project_path, "model", "model.pkl")
+    model = load_model(model_path) 
+
+    df = pd.DataFrame(np.random.rand(10, 108))
+    preds = inference(model, df)
+
+    assert all(x in [0, 1] for x in preds)
+
 
 
 # TODO: implement the second test. Change the function name and input as needed
