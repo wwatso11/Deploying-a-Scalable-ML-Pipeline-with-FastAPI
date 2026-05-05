d02b69c8fcdeba33b6295820e794384576435dd9
wwatso11 <wwatso11@wgu.edu.com>
2026-05-05 00:18:40 +0000

    Project completion

--- main.py
+++ main.py
@@ -26,21 +26,23 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
-encoder = load_model(path)
+project_path = os.path.dirname(os.path.abspath(__file__))
 
-path = None # TODO: enter the path for the saved model 
-model = load_model(path)
+encoder_path = os.path.join(project_path, "model", "encoder.pkl")# TODO: enter the path for the saved encoder 
+encoder = load_model(encoder_path)
+
+model_path = os.path.join(project_path, "model", "model.pkl") # TODO: enter the path for the saved model 
+model = load_model(model_path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
+    return {"message": "Welcome to the Census Income Prediction API"}
 
 
 # TODO: create a POST on a different path that does model inference
@@ -65,10 +67,14 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
+        data,
+        categorical_features=cat_features,
+        training= False,
+        encoder=encoder,
         # your code here
         # use data as data input
         # use training = False
         # do not need to pass lb as input
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed) # your code here to predict the result using data_processed
     return {"result": apply_label(_inference)}
